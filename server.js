6d71046fbc0627694c654c1a963925b22a6391b9
jianghai <liujh@hungraim.com>
2019-04-02 15:41:24 +0800

    从腾讯云服务器中同步了代码

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app
     server.get('*', (req, res) => {
       return handle(req, res);
     });
-    server.listen(3000, err => {
+    server.listen(8090, err => {
       if (err) throw err;
-      console.log('> Ready on http://localhost:3000');
     });
+    console.log('> Ready on http://localhost:8090');
   })
   .catch(ex => {
     console.log(ex.stack);
